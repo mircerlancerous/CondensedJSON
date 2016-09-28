2446e0adf9dca7c896e8755a4874b1d54b4038a1
OffTheBricks <mircerlancerous@users.noreply.github.com>
2016-09-28 14:27:15 -0700

    Update CJSON.class.php
    improved handling of more complex object structures and array content

--- CJSON.class.php
+++ CJSON.class.php
@@ -2,11 +2,13 @@
 class CJSON{
 	public function CJSON(){}
 	public static function stringify($inObj){
-		return CJSON::encodeJSON($inObj);
+		return CJSON::encode($inObj);
 	}
-	public static function encodeJSON($inObj){
+	public static function encode($inObj){
 		$newObj = CJSON::copyObject($inObj);
 		$foundArr = FALSE;
+		$head = "C";
+		
 		if(CJSON::checkProperty($newObj)){
 			$newObj = CJSON::compressArray($newObj);
 			$foundArr = TRUE;
@@ -18,6 +20,10 @@ class CJSON{
 					if(!CJSON::checkProperty($newObj->$property)){
 						continue;
 					}
+					if($head != "C"){
+						$head .= ",";
+					}
+					$head .= $property;
 					//this is an appropriate array so compress it
 					$newObj->$property = CJSON::compressArray($newObj->$property);
 					$foundArr = TRUE;
@@ -25,22 +31,28 @@ class CJSON{
 			}
 		}
 		if($foundArr){
-			return "C".json_encode($newObj);
+			return $head.json_encode($newObj);
 		}
 		return json_encode($newObj);
 	}
 	public static function parse($jsonStr){
-		return CJSON::decodeJSON($jsonStr);
+		return CJSON::decode($jsonStr);
 	}
-	public static function decodeJSON($jsonStr){
+	public static function decode($jsonStr){
 		//if the first character is not a 'C' then this is regular JSON
 		$firstChar = substr($jsonStr,0,1);
 		if($firstChar != 'C'){
 			return json_decode($jsonStr);
 		}
-		$jsonStr = substr($jsonStr,1);
+		$pos = strpos($jsonStr,"{");
+		$pos2 = strpos($jsonStr,"[");
+		if(($pos === FALSE && $pos2 !== FALSE) || ($pos2 !== FALSE && $pos2 < $pos)){
+			$pos = $pos2;
+		}
+		$mapData = explode(",",substr($jsonStr,1,$pos-1));
+		$jsonStr = substr($jsonStr,$pos);
 		$inObj = json_decode($jsonStr);
-		if(CJSON::checkProperty($inObj)){
+		if(CJSON::checkProperty($inObj,FALSE)){
 			$inObj = CJSON::uncompressArray($inObj);
 		}
 		else{
@@ -50,6 +62,9 @@ class CJSON{
 					if(!CJSON::checkProperty($inObj->$property,FALSE)){
 						continue;
 					}
+					if(!in_array($property,$mapData)){
+						continue;
+					}
 					//this is an appropriate array so uncompress it
 					$inObj->$property = CJSON::uncompressArray($inObj->$property);
 				}
@@ -57,15 +72,9 @@ class CJSON{
 		}
 		return $inObj;
 	}
-	private static function convertArrayToObject($inArr){
-		$outObj = new stdClass();
-		foreach($inArr as $key => $item){
-			$outObj->$key = $item;
-		}
-		return $outObj;
-	}
+	
 	private static function checkProperty($property,$compress=TRUE){
-		if(!is_array($property)){
+		if(!is_array($property) && !is_object($property)){
 			return FALSE;
 		}
 		//if the array is empty or just one row then skip it
@@ -73,9 +82,9 @@ class CJSON{
 			return FALSE;
 		}
 		//check if this is a 2d array.  Skip if not
-	/*	reset($property);
+		reset($property);
 		$firstKey = key($property);
-		if(!is_array($property[$firstKey])){
+		if(!is_array($property[$firstKey]) && !is_object($property[$firstKey])){
 			return FALSE;
 		}
 		//if we're uncompressing fewer checks are possible/required
@@ -91,16 +100,26 @@ class CJSON{
 		$firstKey = key($property[$firstKey]);
 		if(is_numeric($firstKey) && $firstKey == 0){
 			return FALSE;
-		}*/
+		}
 		return TRUE;
 	}
 	public static function compressArray($inArr){
-		if(!array_key_exists(0,$inArr)){
+		$keys = array();
+		$tempArr = array();
+		if(!is_array($inArr[0])){
+			$tempArr = $inArr[0];
+		}
+		else{
+			$tempArr = @get_object_vars($inArr[0]);
+		}
+		if(!$tempArr){
 			return $inArr;
 		}
-		$keys = array();
-		foreach($inArr[0] as $key => $item){
-		    $keys[] = $key;
+		foreach($tempArr as $key => $item){
+			$keys[] = $key;
+		}
+		if(!$keys){
+			return $inArr;
 		}
 		$final = array($keys);
 		foreach($inArr as $rowArr){
